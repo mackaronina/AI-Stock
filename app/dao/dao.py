66342df6e8c74bed848bf62efcf7c492058b2297
mackaronina <mihamishu123@gmail.com>
2025-12-05 20:37:13 +0200

    Added text search to the page with all images

--- app/dao/dao.py
+++ app/dao/dao.py
@@ -16,19 +16,23 @@ class ImageDAO(BaseDAO[Image]):
 
     @classmethod
     @connection
-    async def find_all_with_sort(cls, session: AsyncSession, sort_by: Literal['date', 'likes'] = 'likes',
-                                 order_by: Literal['asc', 'desc'] = 'desc', **filter_by) -> Sequence[Image]:
+    async def find_all_with_filters(cls, session: AsyncSession, sort_by: Literal['date', 'likes'] = 'likes',
+                                    order_by: Literal['asc', 'desc'] = 'desc', term: str = None,
+                                    **filter_by) -> Sequence[Image]:
+        query = select(Image).filter_by(**filter_by)
+        if term is not None:
+            query = query.where(Image.prompt.contains(term))
         if sort_by == 'date':
             if order_by == 'desc':
-                query = select(Image).filter_by(**filter_by).order_by(desc(Image.created_at))
+                query = query.order_by(desc(Image.created_at))
             else:
-                query = select(Image).filter_by(**filter_by).order_by(asc(Image.created_at))
+                query = query.order_by(asc(Image.created_at))
         elif sort_by == 'likes':
             if order_by == 'desc':
-                query = select(Image).filter_by(**filter_by).outerjoin(Like).group_by(Image.id).order_by(
+                query = query.outerjoin(Like).group_by(Image.id).order_by(
                     desc(func.count(Like.id)))
             else:
-                query = select(Image).filter_by(**filter_by).outerjoin(Like).group_by(Image.id).order_by(
+                query = query.filter_by(**filter_by).outerjoin(Like).group_by(Image.id).order_by(
                     asc(func.count(Like.id)))
         result = await session.execute(query)
         records = result.scalars().all()
--- app/schemas.py
+++ app/schemas.py
@@ -1,29 +1,42 @@
 import uuid
-from typing import Self, Literal
+from typing import Self, Literal, Annotated
 
-from pydantic import BaseModel, EmailStr, Field, model_validator
+from pydantic import BaseModel, EmailStr, Field, model_validator, ConfigDict, AfterValidator
 
 
 # Field description is the message that will appear when a validation error occurs
-class RequestSearchQuery(BaseModel):
+class Base(BaseModel):
+    model_config = ConfigDict(str_strip_whitespace=True)
+
+
+def validate_search_term(value: str | None) -> str | None:
+    if value is None:
+        return None
+    if 3 <= len(value) <= 300:
+        return value
+    return None
+
+
+class RequestSearchQuery(Base):
     sort_by: Literal['date', 'likes'] = 'likes'
     order_by: Literal['asc', 'desc'] = 'desc'
+    term: Annotated[str | None, AfterValidator(validate_search_term)] = None
 
 
-class RequestGenerateImage(BaseModel):
+class RequestGenerateImage(Base):
     prompt: str = Field(min_length=3, max_length=200, description='Prompt must be between 3 and 200 characters long')
 
 
-class RequestPlaceLike(BaseModel):
+class RequestPlaceLike(Base):
     to_image_id: uuid.UUID = Field(description='The id of the image being liked must be valid')
 
 
-class RequestLogin(BaseModel):
+class RequestLogin(Base):
     username: str = Field(min_length=4, max_length=20, description='Username must be between 4 and 20 characters long')
     password: str = Field(min_length=6, max_length=20, description='Password must be between 6 and 20 characters long')
 
 
-class RequestRegister(BaseModel):
+class RequestRegister(Base):
     username: str = Field(min_length=4, max_length=20, description='Username must be between 4 and 20 characters long')
     email: EmailStr = Field(description='Email address must be valid')
     password: str = Field(min_length=6, max_length=20, description='Password must be between 6 and 20 characters long')
